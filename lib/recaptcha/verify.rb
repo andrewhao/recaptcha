964e4d455347a935c78e69d1730b3502ab306629
Andrew Hao <andrewhao@gmail.com>
2016-05-19 19:51:48 -0700

    Adding extra puts

--- lib/recaptcha/verify.rb
+++ lib/recaptcha/verify.rb
@@ -81,6 +81,7 @@ module Recaptcha
 
     def recaptcha_error(model, attribute, message, key, default)
       message = message || Recaptcha.i18n(key, default)
+      puts "[recaptcha_error]: #{message}"
       if model
         model.errors.add attribute, message
       else
